1c426cbb95eb9c24e3c9147c67250705a52d3363
Yurii Butkov <herr.butkov@gmail.com>
2026-01-13 23:43:16 +0100

    add color of light

--- includes/colors.h
+++ includes/colors.h
@@ -6,15 +6,15 @@
 /*   By: ybutkov <ybutkov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/31 16:34:08 by ybutkov           #+#    #+#             */
-/*   Updated: 2026/01/05 18:07:05 by ybutkov          ###   ########.fr       */
+/*   Updated: 2026/01/13 21:45:40 by ybutkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COLORS_H
 # define COLORS_H
 
-# include "vectors.h"
 # include "rays.h"
+# include "vectors.h"
 
 # define COLOR_DEFAULT "0xFFFFFF"
 # define COLOR_WHITE 0xFFFFFF
@@ -40,6 +40,7 @@ t_color			create_color(int r, int g, int b);
 t_color			color_add(t_color c1, t_color c2);
 t_color			color_mix(t_color c1, t_color c2, double intensity);
 t_color			color_mult(t_color c1, double scalar);
+t_color			color_product(t_color c1, t_color c2);
 int				color_to_int(t_color color);
 t_color			get_background_color(t_ray ray);
 
--- src/math/color/color_core.c
+++ src/math/color/color_core.c
@@ -6,7 +6,7 @@
 /*   By: ybutkov <ybutkov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/02 23:20:16 by ybutkov           #+#    #+#             */
-/*   Updated: 2026/01/07 14:48:23 by ybutkov          ###   ########.fr       */
+/*   Updated: 2026/01/13 22:06:34 by ybutkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,16 @@ t_color	color_add(t_color c1, t_color c2)
 	color.r = c1.r + c2.r;
 	color.g = c1.g + c2.g;
 	color.b = c1.b + c2.b;
+	return (color);
+}
+
+t_color	color_product(t_color c1, t_color c2)
+{
+	t_color	color;
+
+	color.r = c1.r * c2.r;
+	color.g = c1.g * c2.g;
+	color.b = c1.b * c2.b;
 	return (color);
 }
 
