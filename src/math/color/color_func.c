256946ff7fc7690b83c24cbc1cffb83fece63dde
Yurii Butkov <herr.butkov@gmail.com>
2026-01-25 20:46:49 +0100

    refactoring background_color

--- src/math/color/color_func.c
+++ src/math/color/color_func.c
@@ -6,7 +6,7 @@
 /*   By: ybutkov <ybutkov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 19:47:47 by ybutkov           #+#    #+#             */
-/*   Updated: 2026/01/25 20:14:50 by ybutkov          ###   ########.fr       */
+/*   Updated: 2026/01/25 20:45:47 by ybutkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,22 +43,13 @@ t_color	get_background_color(t_ray ray, t_map *map)
 	double		v;
 	double		theta;
 	double		phi;
-	t_vec3		dir;
-	t_color		color;
-	double		t;
 
 	if (!map || !map->background_texture)
-	{
-		t = 0.5 * (ray.direction.y + 1.0);
-		color.r = (1.0 - t) * get_bg_color_bottom().r + t * get_bg_color_top().r;
-		color.g = (1.0 - t) * get_bg_color_bottom().g + t * get_bg_color_top().g;
-		color.b = (1.0 - t) * get_bg_color_bottom().b + t * get_bg_color_top().b;
-		return (color);
-	}
+		return (color_mix(get_bg_color_bottom(), get_bg_color_top(),
+				0.5 * (ray.direction.y + 1.0)));
 	tex = (t_texture *)map->background_texture;
-	dir = vector_norm(ray.direction);
-	theta = acos(-dir.y);
-	phi = atan2(-dir.z, dir.x) + M_PI;
+	theta = acos(-ray.direction.y);
+	phi = atan2(-ray.direction.z, ray.direction.x) + M_PI;
 	u = phi / (2.0 * M_PI);
 	v = theta / M_PI;
 	return (get_texture_color(tex, u, v));
